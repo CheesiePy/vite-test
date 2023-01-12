c4c6e8213fbabca84c5576cbb42a1f1f99d1c684
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-12 21:22:24 +0200

    update 12.01.23

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import javascriptLogo from './javascript.svg'
 import { setupCounter, setupLinkToggle } from './counter.js'
 import { setupDarkModeToggle } from './modules/dark-mode.js'
 import typescriptLogo from './Typescript_logo.svg'
+let logged_in = false;
 
-document.querySelector('#app').innerHTML = `
+if(logged_in == true){
+  document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -23,6 +25,24 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
+}else{
+  document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div class="login-form">
+      <h1>Log In</h1>
+      <div id="login">
+          <form>
+              <input type="text" id="username" name="username" placeholder="Username">
+              <input type="password" id="password" name="password" placeholder="Password">
+          </form>
+              <button onclick="login()">Log In</button>
+      </div>
+    </div>
+  </div>
+  `
+
+}
+
 setupLinkToggle(document.querySelector("#vanil"), `${typescriptLogo}`);
 setupCounter(document.querySelector('#counter'))
 setupDarkModeToggle(document.querySelector('#dark-mode'))
