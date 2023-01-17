cefb4982c32f0d73e9952add0b87bdfc6909f42a
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-17 18:54:47 +0200

    update NavBar

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import { setupCounter, setupLinkToggle } from './counter.js'
-import { setupDarkModeToggle } from './modules/dark-mode.js'
-import typescriptLogo from './Typescript_logo.svg'
-let logged_in = false;
+import { setupNavBar } from './components/NavBar.js'
 
-if(logged_in == true){
+
+
   document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -17,7 +15,6 @@ if(logged_in == true){
     <h1 id="hd">Hello world!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
-      <button id="dark-mode" type="button">Dark-mode</button>
     </div
     <p class="read-the-docs">
     <
@@ -25,26 +22,6 @@ if(logged_in == true){
     </p>
   </div>
 `
-}else{
-  document.querySelector('#app').innerHTML = `
-  <div class="container">
-    <div class="login-form">
-      <h1>Log In</h1>
-      <div id="login">
-          <form>
-              <input type="text" id="username" name="username" placeholder="Username">
-              <input type="password" id="password" name="password" placeholder="Password">
-          </form>
-              <button onclick="login()">Log In</button>
-      </div>
-    </div>
-  </div>
-  `
 
-}
-
-setupLinkToggle(document.querySelector("#vanil"), `${typescriptLogo}`);
-setupCounter(document.querySelector('#counter'))
-setupDarkModeToggle(document.querySelector('#dark-mode'))
-$('#hd').css("color", "blue")
-
+const navbar = document.querySelector('#navbar')
+setupNavBar(navbar)
